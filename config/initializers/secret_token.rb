c8f4e5353ebaf0913d313bf603b724e982383dcf
ilstar <xingqinglzq@gmail.com>
2012-02-27 10:59:00 +0800

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OFLunch::Application.config.secret_token = '5abb8bb1cc0f7c05f4ef46763541ff5f4b0e386140653a3f980219edfcf4ec3ddbcd6530c5416e9628ccccd369847c1fc62077a7c3474caeda00d6c1cb51277b'
