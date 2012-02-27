8422dfb770059271e362a591e6917d8039f9337d
ilstar <xingqinglzq@gmail.com>
2012-02-27 11:49:51 +0800

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 OFLunch::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create users
+pwd = "openfeint"
+User.create! name: "Andy Yao", email: "andy.yao@gree.co.jp", password: pwd
+User.create! name: "Kevin Xu", email: "kevin.xu@gree.co.jp", password: pwd
+User.create! name: "Fred Liang", email: "fred.liang@gree.co.jp", password: pwd
+User.create! name: "Richard Huang", email: "richard.huang@gree.co.jp", password: pwd
+User.create! name: "Jason Lai", email: "jason.lai@gree.co.jp", password: pwd
+User.create! name: "Jian Fu", email: "jian.fu@gree.co.jp", password: pwd
+User.create! name: "Bo Fu", email: "bo.fu@gree.co.jp", password: pwd
+User.create! name: "Vincent Xie", email: "vincent.xie@gree.co.jp", password: pwd
+User.create! name: "lay.zhu", email: "lay.zhu@gree.co.jp", password: pwd
+User.create! name: "Xingkui Wang", email: "xingkui.wang@gree.co.jp", password: pwd
+User.create! name: "Yue Tian", email: "yue.tian@gree.co.jp", password: pwd
+User.create! name: "Tiancong Pang", email: "tiancong.pang@gree.co.jp", password: pwd
+User.create! name: "He Tian", email: "he.tian@gree.co.jp", password: pwd
+User.create! name: "Ray Huang", email: "ray.huang@gree.co.jp", password: pwd
+User.create! name: "Chang Cheng", email: "chang.cheng@gree.co.jp", password: pwd
+User.create! name: "Rong Yang", email: "rong.yang@gree.co.jp", password: pwd
+User.create! name: "Yu Hu", email: "yu.hu@gree.co.jp", password: pwd
