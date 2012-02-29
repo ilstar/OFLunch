a773e2ee48bcba528f32778e512539a756b93d89
ilstar <xingqinglzq@gmail.com>
2012-02-29 10:50:23 +0800

    add meal_time, menu_of_meal

---
+++ db/migrate/20120229021823_create_meal_times.rb
@@ -0,0 +1,8 @@
+class CreateMealTimes < ActiveRecord::Migration
+  def change
+    create_table :meal_times do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/meal_time_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe MealTime do
+  it { should have_many(:menu_of_meals).dependent(:destroy) }
+  it { should have_many(:vendors).through(:menu_of_meals) }
+end
