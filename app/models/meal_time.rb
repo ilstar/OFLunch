f57f3e799decf03571e46638da14ec2b43be5bce
ilstar <xingqinglzq@gmail.com>
2012-03-01 11:44:24 +0800

    re-implement adding mom

--- app/models/meal_time.rb
+++ app/models/meal_time.rb
@@ -10,7 +10,24 @@ class MealTime < ActiveRecord::Base
     where(["created_at > ? AND created_at < ?", Time.current.beginning_of_day, Time.current.end_of_day]).first
   end
 
+  def vendor_ids
+    self.vendors.map &:id
+  end
   def vendor_ids=(ids)
-    puts ids
+    self.vendors = Vendor.find ids.reject(&:blank?)
+  end
+
+  validate :less_than_two_vendors_choosed 
+  validate :no_more_than_one_meal_per_day
+
+  protected
+
+  def less_than_two_vendors_choosed
+    self.errors.add :vendor_ids, "no more than 2 vendors." if self.vendors.size > 2
+    self.errors.add :vendor_ids, "no vendor selected" if self.vendors.size == 0
+  end
+
+  def no_more_than_one_meal_per_day
+    self.errors.add :base, "no more than one meal per day" if self.class.today 
   end
 end
--- app/models/menu_of_meal.rb
+++ app/models/menu_of_meal.rb
@@ -2,13 +2,4 @@ class MenuOfMeal < ActiveRecord::Base
   belongs_to :meal_time
   belongs_to :vendor
 
-  validate :less_than_two_vendors_choosed
-
-  protected
-
-  def less_than_two_vendors_choosed
-    if self.meal_time.menu_of_meals.count >= 2
-      self.errors.add :base, "can't adding more than 2."
-    end
-  end
 end
