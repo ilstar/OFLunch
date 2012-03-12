bd09966a42bc4ebdff72cfd2b629ddcbf6b36151
Andy Yao <andyaoy@gmail.com>
2012-03-12 13:51:13 +0800

    Fix: Can not access log out link after click setting button after lock order list

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,8 +1,14 @@
 class OrderItemsController < ApplicationController
   skip_before_filter :authenticate_user!
+  layout :determine_layout
 
   def index
     @mom = MealTime.today
     @today_order_items = @mom.try(:order_items) || []
+  end
+
+  private
+  def determine_layout
+    current_user ? "application" : "blank"
   end
 end
