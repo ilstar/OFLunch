b5d3dd13e7bf507280f9780e70cbba3f7e717c82
ilstar <xingqinglzq@gmail.com>
2012-05-24 16:53:28 +0800

    only load necessary data in bills calendar

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,13 +1,13 @@
 class BillsController < ApplicationController
   def index
-    @balance_logs = current_user.balance.balance_logs.order('id DESC')
-    @orders = current_user.orders.order('id DESC')
-    @bills = @balance_logs + @orders
-
     if params[:year] and params[:month]
       @date = Date.civil(params[:year].to_i, params[:month].to_i)
     else
       @date = Date.today
     end
+
+    @balance_logs = current_user.balance.balance_logs.where(created_at: (@date.beginning_of_month..@date.end_of_month))
+    @orders = current_user.orders.where(created_at: (@date.beginning_of_month..@date.end_of_month))
+    @bills = @balance_logs + @orders
   end
 end
