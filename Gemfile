072b1dd0aa8d3a38e95913e4b340a03111e4b4eb
ilstar <xingqinglzq@gmail.com>
2012-05-29 11:31:37 +0800

    add feature: exception notification

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'rails3_acts_as_paranoid', '0.2.2'
 
 gem 'whenever', '0.7.3'
 
+gem 'exception_notification', '~> 2.6.1'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,9 @@ OFLunch::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.use ExceptionNotifier,
+    email_prefix: "[oflunch-exception]",
+    sender_address: %w{exception_notifier},
+    exception_recipients: %w{fred.liang@gree.co.jp}
 end
