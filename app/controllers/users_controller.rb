0b6049e30b1216b3cac23a1e3e3dca3a58fc1389
Fred Liang <fred.liang@gree.co.jp>
2012-08-13 11:59:19 +0800

    #26 fix bug: user can't update other fields in edit&edit_password pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    reject_other_fields_for_update
     if @user.update_attributes(params[:user])
-      redirect_to edit_user_path, :notice => "修改成功"
+      redirect_to edit_user_url, :notice => "修改成功"
     else
-      render :action => "edit"
+      render "edit"
     end
   end
 
   def update_password
+    reject_other_fields_for_update_password
     if @user.update_with_password(params[:user])
-      redirect_to edit_password_user_path, :notice => "修改成功"
+      redirect_to edit_password_user_url, :notice => "修改成功"
     else
       render "edit_password"
     end
@@ -30,6 +32,14 @@ class UsersController < ApplicationController
 
   def load_current_user
     @user = current_user
+  end
+
+  def reject_other_fields_for_update
+    params[:user].reject! { |k, v| not %w{name}.include?(k) }
+  end
+
+  def reject_other_fields_for_update_password
+    params[:user].reject! { |k, v| not %w{current_password password password_confirmation}.include?(k) }
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe UsersController do
 	before do
-	  @user = FactoryGirl.create(:user)
+		@password = 'abcdefjj'
+	  @user = FactoryGirl.create(:user, password: @password)
 
 	  sign_in @user
 	end
@@ -22,5 +23,65 @@ describe UsersController do
 	    response.should be_success
 	  end
 	end
+
+  describe "PUT update" do
+    describe "with valid params" do
+      it "updates name successfully" do
+        new_name = 'newnameneverseenbefore'
+
+        put :update, {user: {name: new_name}}
+
+        assigns(:user).name.should == new_name
+      end
+
+      it "redirects to edit template" do
+        put :update, {user: {name: 'foo'}}
+
+        response.should redirect_to(edit_user_url)
+      end
+
+      it "can't update other fields" do
+        put :update, {user: {name: 'foo', email: 'newmail@mail.com'}}
+
+        assigns(:user).email.should == @user.email
+      end
+    end
+
+    describe "with invalid params" do
+      it "rendered edit template" do
+        put :update, {user: {name: ''}}
+
+        response.should render_template('edit')
+      end
+    end
+  end
+
+  describe "PUT update_password" do
+    describe "with valid password" do
+      it "has new correct password" do
+        new_password = 'blabla234'
+
+        put :update_password, user: {current_password: @password, password: new_password, password_confirmation: new_password}
+
+        assigns(:user).valid_password?(new_password).should be_true
+      end
+
+      it "redirect to edit_password page" do
+        new_password = 'blabla234'
+
+        put :update_password, user: {current_password: @password, password: new_password, password_confirmation: new_password}
+
+        response.should redirect_to(edit_password_user_url)
+      end
+    end
+
+    describe "with invalid password" do
+      it "rendered edit password template" do
+        put :update_password, user: {current_password: '', password: '', password_confirmation: ''}
+
+        response.should render_template('edit_password')
+      end
+    end
+  end
 
 end
