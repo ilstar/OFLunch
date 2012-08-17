d4d19bf6a883d1320b48d5591a886750b7d082f6
Fred Liang <fred.liang@gree.co.jp>
2012-08-17 16:09:50 +0800

    #34 refactor: using categories_with_menu_items method in menu_items controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,12 +1,7 @@
 class MenuItemsController < ApplicationController
   def index
     vendor = Vendor.find params[:vendor_id]
-    @menu_items_group_category = {}
-    
-    vendor.menu_items.each do |menu_item|
-      @menu_items_group_category[menu_item.category] ||= []
-      @menu_items_group_category[menu_item.category] << menu_item
-    end
+    @categories_with_menu_items = vendor.categories_with_menu_items
 
     respond_to do |format|
       format.js
