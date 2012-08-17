1de961e0a2233deba537723aadd98f91bf95787a
Fred Liang <fred.liang@gree.co.jp>
2012-08-17 09:59:26 +0800

    #34 add category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[
+  ['送餐费', 'freight'],
+  ['主食', ''],
+  ['饮料', ''],
+  ['凉菜', ''],
+  ['热菜', ''],
+  ['套餐', ''],
+].each do |attr|
+  Category.create! name: attr[0], identifier: attr[1]
+end
