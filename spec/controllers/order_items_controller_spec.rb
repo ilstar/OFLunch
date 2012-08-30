bb4dfc6af3b6a87bab692ac6e32d4193d28cfec0
Fred Liang <fred.liang@gree.co.jp>
2012-08-30 18:16:01 +0800

    #33 display vendors and menu items even if deleted some vendor and menu items

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -12,13 +12,16 @@ describe OrderItemsController do
   describe "GET 'index'" do
     before do
       @meal_time = FactoryGirl.create(:meal_time)
-      vendor = FactoryGirl.create(:vendor)
-      menu_item = FactoryGirl.create(:menu_item, vendor: vendor)
-      @meal_time.vendors << vendor
-      order = FactoryGirl.create(:order, user: @user, meal_time: @meal_time)
-      order_item = FactoryGirl.create(:order_item, order: order, menu_item: menu_item)
-      other_order = FactoryGirl.create(:order, meal_time: @meal_time)
-      order_item = FactoryGirl.create(:order_item, order: other_order, menu_item: menu_item)
+
+      @vendor = FactoryGirl.create(:vendor)
+      @menu_item = FactoryGirl.create(:menu_item, vendor: @vendor)
+
+      @meal_time.vendors << @vendor
+
+      @order = FactoryGirl.create(:order, user: @user, meal_time: @meal_time)
+      @order_item = FactoryGirl.create(:order_item, order: @order, menu_item: @menu_item)
+      @other_order = FactoryGirl.create(:order, meal_time: @meal_time)
+      @order_item = FactoryGirl.create(:order_item, order: @other_order, menu_item: @menu_item)
     end
 
     it "success when have no mom" do
@@ -44,12 +47,39 @@ describe OrderItemsController do
       response.should be_success
     end
 
-    it "render when meal_time is close" do
-      @meal_time.close!
+    describe "when meal_time is closed" do
+      it "renders template as normal" do
+        @meal_time.close!
 
-      get :index
+        get :index
 
-      response.should be_success
+        response.should be_success
+      end
+
+      it "renders template after admin deleted a vendor and a menu_item" do
+        # prepare data
+        # 3 people order 3 menu_items from 3 different vendors
+        @vendor2 = FactoryGirl.create(:vendor)
+        @menu_item2 = FactoryGirl.create(:menu_item, vendor: @vendor2)
+        @vendor3 = FactoryGirl.create(:vendor)
+        @menu_item3 = FactoryGirl.create(:menu_item, vendor: @vendor3)
+        @meal_time.vendors << @vendor2
+        @meal_time.vendors << @vendor3
+
+        @order2 = FactoryGirl.create :order, meal_time: @meal_time
+        @order_item2 = FactoryGirl.create :order_item, order: @order2, menu_item: @menu_item2
+        @order3 = FactoryGirl.create :order, meal_time: @meal_time
+        @order_item3 = FactoryGirl.create :order_item, order: @order3, menu_item: @menu_item3
+
+        # delete vendor 2 and menu_item 3
+        @vendor2.destroy
+        @menu_item3.destroy
+        @meal_time.close!
+
+        get :index
+
+        response.should be_success
+      end
     end
 
     it "successful render when visit old meal_time by date" do
